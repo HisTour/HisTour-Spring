2f5cfc8a256d36922cd80f34fa8a5e55cfaea776
thguss <thgus345@naver.com>
2024-08-23 18:29:27 +0900

    fix: updated description

--- histour-application/src/main/java/trible/histour/application/port/input/dto/request/auth/SignInRequest.java
+++ histour-application/src/main/java/trible/histour/application/port/input/dto/request/auth/SignInRequest.java
@@ -5,7 +5,7 @@ import trible.histour.application.domain.member.SocialType;
 
 @Schema(description = "소셜 로그인 요청 정보")
 public record SignInRequest(
-	@Schema(description = "가입하려는 소셜 서비스", example = "KAKAO")
+	@Schema(description = "로그인하려는 소셜 서비스", example = "KAKAO")
 	SocialType type
 ) {
 }
