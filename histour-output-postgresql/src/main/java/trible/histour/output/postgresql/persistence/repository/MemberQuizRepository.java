774fa5edecab7dbeae1dca8c21363e9e3f196790
thguss <thgus345@naver.com>
2024-08-25 15:39:59 +0900

    fix: fixed parameter name

--- histour-output-postgresql/src/main/java/trible/histour/output/postgresql/persistence/repository/MemberQuizRepository.java
+++ histour-output-postgresql/src/main/java/trible/histour/output/postgresql/persistence/repository/MemberQuizRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import trible.histour.output.postgresql.persistence.entity.MemberQuizEntity;
 
 public interface MemberQuizRepository extends JpaRepository<MemberQuizEntity, Long> {
-	List<MemberQuizEntity> findAllByMemberIdAndQuizIdIn(long memberId, List<Long> ids);
+	List<MemberQuizEntity> findAllByMemberIdAndQuizIdIn(long memberId, List<Long> quizIds);
 }
