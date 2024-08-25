aa0b41fc3f06b84840287f09b502ec123c985f40
thguss <thgus345@naver.com>
2024-08-25 18:44:01 +0900

    feat: character update api

--- histour-application/src/main/java/trible/histour/application/domain/member/MemberService.java
+++ histour-application/src/main/java/trible/histour/application/domain/member/MemberService.java
@@ -1,18 +1,24 @@
 package trible.histour.application.domain.member;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import trible.histour.application.port.input.MemberUseCase;
 import trible.histour.application.port.output.persistence.MemberPort;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService implements MemberUseCase {
 	private final MemberPort memberPort;
 
+	@Transactional
 	@Override
-	public void registerProfile() {
-		//TODO: implementation
+	public void updateCharacter(long memberId, long characterId) {
+		val member = memberPort.findById(memberId);
+		member.updateCharacter(characterId);
+		memberPort.update(member);
 	}
 }
