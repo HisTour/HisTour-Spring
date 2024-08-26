93b168195c417f776014d13e7b25c8df78bfe62b
thguss <thgus345@naver.com>
2024-08-26 14:11:28 +0900

    chore: rollback

--- histour-application/src/main/java/trible/histour/application/port/output/persistence/MemberQuizPort.java
+++ histour-application/src/main/java/trible/histour/application/port/output/persistence/MemberQuizPort.java
@@ -1,4 +1,9 @@
 package trible.histour.application.port.output.persistence;
 
+import java.util.List;
+
+import trible.histour.application.domain.quiz.MemberQuiz;
+
 public interface MemberQuizPort {
+	List<MemberQuiz> findAllByMemberIdAndQuizIds(long memberId, List<Long> quizIds);
 }
--- histour-output-postgresql/src/main/java/trible/histour/output/postgresql/adapter/MemberQuizAdapter.java
+++ histour-output-postgresql/src/main/java/trible/histour/output/postgresql/adapter/MemberQuizAdapter.java
@@ -1,13 +1,23 @@
 package trible.histour.output.postgresql.adapter;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import trible.histour.application.domain.quiz.MemberQuiz;
 import trible.histour.application.port.output.persistence.MemberQuizPort;
+import trible.histour.output.postgresql.persistence.entity.MemberQuizEntity;
 import trible.histour.output.postgresql.persistence.repository.MemberQuizRepository;
 
 @Component
 @RequiredArgsConstructor
 public class MemberQuizAdapter implements MemberQuizPort {
 	private final MemberQuizRepository memberQuizRepository;
+
+	@Override
+	public List<MemberQuiz> findAllByMemberIdAndQuizIds(long memberId, List<Long> quizIds) {
+		return memberQuizRepository.findAllByMemberIdAndQuizIdIn(memberId, quizIds).stream()
+			.map(MemberQuizEntity::toDomain).toList();
+	}
 }
