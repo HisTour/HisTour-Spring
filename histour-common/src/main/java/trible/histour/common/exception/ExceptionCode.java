876c72039b58a6971254c938b8caa68a87f00d8b
thguss <thgus345@naver.com>
2024-09-11 01:08:58 +0900

    add: added NO_CHARACTER exception

--- histour-common/src/main/java/trible/histour/common/exception/ExceptionCode.java
+++ histour-common/src/main/java/trible/histour/common/exception/ExceptionCode.java
@@ -9,11 +9,13 @@ public enum ExceptionCode {
 	// 4xx
 	UNAUTHORIZED(401, "유효하지 않은 토큰"),
 	BAD_REQUEST(400, "유효하지 않은 요청"),
+	NO_CHARACTER(400, "캐릭터 설정이 필요한 회원"),
 	NOT_FOUND(404, "존재하지 않는 자원"),
 
 	// 5xx
 	INTERNAL_SERVER_ERROR(500, "서버 내부 오류"),
-	SERVICE_AVAILABLE(503, "서비스에 접근할 수 없음"),;
+	SERVICE_AVAILABLE(503, "서비스에 접근할 수 없음");
+
 	private final int statusCode;
 	private final String message;
 }
