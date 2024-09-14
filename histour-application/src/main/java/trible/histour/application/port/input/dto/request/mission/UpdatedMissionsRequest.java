ed3d1963d9b8f41b60826d6f6bbff4bfc014478d
thguss <thgus345@naver.com>
2024-09-15 03:18:22 +0900

    add: added description

--- histour-application/src/main/java/trible/histour/application/port/input/dto/request/mission/UpdatedMissionsRequest.java
+++ histour-application/src/main/java/trible/histour/application/port/input/dto/request/mission/UpdatedMissionsRequest.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "미션 해금 요청 정보")
 public record UpdatedMissionsRequest(
+	@Schema(description = "완료한 서브미션 id", example = "1")
 	long completedMissionId,
+	@Schema(description = "선택된 다음 서브미션 id", example = "1")
 	long nextMissionId
 ) {
 }
